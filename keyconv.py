f9aee4d2d9acb69caeb892a63c52900c2a21e969
Matthias Hannig <mail@matthiashannig.de>
2019-12-22 18:57:07 +0100

    added ssh key converter

---
+++ keyconv.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+Convert ssh keys to BER format required by
+HP Comware 5.
+"""
+
+import subprocess
+import binascii
+import textwrap
+import argparse
+
+
+def parse_args():
+    """Parse commandline arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("keyfile")
+
+    return parser.parse_args()
+
+
+def convert_ber(keyfile):
+    """
+    Convert an ssh key stored in a keyfile
+    to BER encoding.
+    """
+    ssh_keygen = subprocess.Popen((
+        "ssh-keygen", "-f", keyfile,
+        "-e", "-m", "PKCS8"), stdout=subprocess.PIPE)
+    result = subprocess.check_output((
+        "openssl", "pkey", "-pubin", "-outform", "DER"),
+        stdin=ssh_keygen.stdout)
+
+    return result
+
+
+def format_key(key):
+    """Format key and wrap after 74 chars."""
+    return "\n".join(
+        textwrap.wrap(
+            str(binascii.hexlify(key), "utf-8").upper(),
+            width=74)) + "\n"
+
+
+def cli_main():
+    """Convert the ssh key provided as file"""
+    args = parse_args()
+    key = convert_ber(args.keyfile)
+    print(format_key(key))
+
+
+if __name__ == "__main__":
+    """Convert a key"""
+    cli_main()
+
